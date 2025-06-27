a3d2bc2b76e0dafff6dbc2f3f37cf5831db96aaf
ki2pixel <33156948+ki2pixel@users.noreply.github.com>
2025-06-27 19:57:04 +0200

    Delete gunicorn_config.py

--- gunicorn_config.py
+++
@@ -1,9 +0,0 @@
-# gunicorn_config.py
-
-def post_fork(server, worker):
-    """
-    Hook Gunicorn qui est appelé après la création d'un processus worker.
-    C'est l'endroit idéal pour démarrer des threads d'arrière-plan.
-    """
-    from app_render import start_background_tasks
-    start_background_tasks()
