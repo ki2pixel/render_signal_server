86f0c3ebc808cfa9ccd960cf53c00102702c23be
ki2pixel <33156948+ki2pixel@users.noreply.github.com>
2025-06-27 20:02:47 +0200

    Add files via upload

---
+++ gunicorn_config.py
@@ -0,0 +1,1 @@
+def post_fork(server, worker):    """    Hook Gunicorn qui est appelé après la création d'un processus worker.    C'est l'endroit idéal pour démarrer des threads d'arrière-plan.    """    from app_render import start_background_tasks    start_background_tasks()
